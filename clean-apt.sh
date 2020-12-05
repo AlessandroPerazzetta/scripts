a43f5a3f8d334bf7594632b586ad7146d4e252fd
AlessandroPerazzetta <alessandro.perazzetta@gmail.com>
2020-12-06 00:19:45 +0100

    Update clean-apt.sh

--- clean-apt.sh
+++ clean-apt.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+systemctl disable systemd-networkd-wait-online.service
+systemctl mask systemd-networkd-wait-online.service
 apt-get remove --purge cloud-* lxd* popularity-contest snapd sosreport iw lxcfs wireless-regdb fuse ntfs-3g open-iscsi open-vm-tools pastebinit
 apt-get clean && apt-get autoclean && apt-get autoremove
 apt-get update && apt-get -f dist-upgrade
