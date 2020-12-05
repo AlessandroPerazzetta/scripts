e8cfe36926bf7aeeee08b9e99d7b12bfe2a98b77
AlessandroPerazzetta <alessandro.perazzetta@gmail.com>
2020-12-06 00:21:22 +0100

    Update clean-apt.sh

--- clean-apt.sh
+++ clean-apt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 systemctl disable systemd-networkd-wait-online.service
 systemctl mask systemd-networkd-wait-online.service
-apt-get remove --purge cloud-* lxd* popularity-contest snapd sosreport iw lxcfs wireless-regdb fuse ntfs-3g open-iscsi open-vm-tools pastebinit
+# Remove wireless support (reinstall kernel required)
+#apt-get remove --purge cloud-* lxd* popularity-contest snapd sosreport iw lxcfs wireless-regdb fuse ntfs-3g open-iscsi open-vm-tools pastebinit
+apt-get remove --purge cloud-* lxd* popularity-contest snapd sosreport lxcfs fuse ntfs-3g open-iscsi open-vm-tools pastebinit
 apt-get clean && apt-get autoclean && apt-get autoremove
 apt-get update && apt-get -f dist-upgrade
